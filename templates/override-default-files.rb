11b72fd3e44118515bdb717150ee8a6b7fd71cdd
Hasarinda Thenuwara <hasarinda.thenuwara@katalyst.com.au>
2023-05-29 09:48:42 +0930

    Prevent spec template file being copied

--- templates/override-default-files.rb
+++ templates/override-default-files.rb
@@ -3,7 +3,7 @@ root.glob("spec/support/**/*.rb").sort.each do |f|
   copy_file(f.relative_path_from(root), force: true)
 end
 
-root.glob("spec/*.rb").sort.each do |f|
+root.glob("spec/*_helper.rb").sort.each do |f|
   copy_file(f.relative_path_from(root), force: true)
 end
 
