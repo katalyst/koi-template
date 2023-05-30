0efe1811745a3713c247a7ea90f93443b850f7b7
Hasarinda Thenuwara <hasarinda.thenuwara@katalyst.com.au>
2023-05-30 11:38:57 +0930

    Add developer support scripts to access ECS services
    * Restore DB from the latest backup
    * Open a SSH console to ECS container
    * Open a SSH console to ECS maintenance container

--- templates/override-default-files.rb
+++ templates/override-default-files.rb
@@ -7,8 +7,10 @@ root.glob("spec/*_helper.rb").sort.each do |f|
   copy_file(f.relative_path_from(root), force: true)
 end
 
-root.glob("bin/*").sort.each do |f|
-  copy_file(f.relative_path_from(root), force: true)
+copy_file("bin/setup", force: true)
+root.glob("bin/ecs-*").sort.each do |f|
+  template(f.relative_path_from(root), force: true)
+  chmod f.relative_path_from(root), 0755
 end
 
 root.glob("public/*.{html,css}").sort.each do |f|
