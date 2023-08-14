3028aaa60ca48efee2fc4d1408fb3c359f48dbeb
Stephen Nelson <stephen@katalyst.com.au>
2023-08-14 12:40:13 +0930

    Suppress CSS warnings from dependencies by default

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -4,3 +4,5 @@ Rails.application.config.dartsass.builds = {
   "application.scss"   => "application.css",
   "koi/koi-admin.scss" => "koi/admin.css",
 }
+
+Rails.application.config.dartsass.build_options = "--quiet-deps"
