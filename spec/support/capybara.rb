980a2dcd965fef96275669fb12381f5d803c92dd
Stephen Nelson <stephen@katalyst.com.au>
2023-09-20 14:18:35 +0930

    Fix cuprite setup
    Cuprite is a built-in test framework and as such takes its arguments
    from ActiveDispatch::SystemTestCase. If we want to change these
    we need to pass them to the register call instead of registering a new
    driver with a built-in name.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,10 +4,6 @@ require "capybara/rspec"
 require "rack_session_access/capybara"
 require "capybara/cuprite"
 
-Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, headless: true, debug: true, window_size: [1920, 1080])
-end
-
 RSpec.configure do |config|
   Capybara.default_driver = Capybara.javascript_driver = :cuprite
 
@@ -17,8 +13,8 @@ RSpec.configure do |config|
   # Rails will set `:selenium` as the runner for system tests by default, but this happens after `before` hooks.
   # We want to use our configured javascript driver and ensure that this is set before
   # our before hooks run so that we can log in (etc).
-  config.before(:all, type: :system) do
-    driven_by Capybara.javascript_driver
+  config.prepend_before(:all, type: :system) do
+    driven_by :cuprite, screen_size: [1920, 1080], options: { headless: true, inspector: false }
   end
 
   config.include Capybara::RSpecMatchers, type: :request
