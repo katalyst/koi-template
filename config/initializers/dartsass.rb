48e16170903564250503ee8ae29cdd06973229bb
Stephen Nelson <stephen@katalyst.com.au>
2024-03-19 11:06:29 +1030

    Move admin css to avoid collisions with Koi-provided entry point

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+Koi.config.admin_stylesheet = "admin"
+
 Rails.application.config.dartsass.builds = {
-  "application.scss"   => "application.css",
-  "koi/koi-admin.scss" => "koi/admin.css",
+  "application.scss" => "application.css",
+  "admin.scss"       => "admin.css",
 }
 
 Rails.application.config.dartsass.build_options = "--quiet-deps"
