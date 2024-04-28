576d5222a31f0124984aec6461abaa18f169348f
Stephen Nelson <stephen@katalyst.com.au>
2024-04-28 21:38:05 +0930

    Remove unnecessary configuration from Admin::ApplicationController
    These configurations are already in Koi::Controller::IsAdminController

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,14 +3,9 @@
 module Admin
   class ApplicationController < ActionController::Base
     include Koi::Controller::IsAdminController
-    include Katalyst::Tables::Backend
-    include Pagy::Backend
 
     authenticate_local_admins Rails.env.development?
 
-    default_form_builder "Koi::FormBuilder"
-
-    helper Katalyst::GOVUK::Formbuilder::Frontend
     helper Koi::ApplicationHelper
     helper Koi::DefinitionListHelper
   end
