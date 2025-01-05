45672ba98243da13a6c868f1955d366684b169ab
Stephen Nelson <stephen@katalyst.com.au>
2025-01-06 10:10:03 +1030

    Fix rubocop warning for Sentry initialisation

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 ActiveSupport.on_load(:active_record) do
-  VERSION = Rails.root.join("VERSION")
+  version = Rails.root.join("VERSION")
 
   # DSN is set via SENTRY_DSN in ENV
   Sentry.init do |config|
     config.enabled_environments = %w[staging production]
-    config.release              = File.read(VERSION).strip.split("/").last if VERSION.exist?
+    config.release              = File.read(version).strip.split("/").last if version.exist?
     filter             = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
     config.before_send = lambda do |event, _hint|
       filter.filter(event.to_hash)
