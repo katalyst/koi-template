15985b1c8a6190fb35daf708205e9741ae70d5cd
Stephen Nelson <stephen@katalyst.com.au>
2025-06-16 14:08:40 +0930

    Remove unnecessary helpers from Admin::ApplicationController
    Koi::Controller already includes this

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,11 +2,6 @@
 
 module Admin
   class ApplicationController < ActionController::Base
-    include Koi::Controller::IsAdminController
-
-    authenticate_local_admins Rails.env.development?
-
-    helper Koi::ApplicationHelper
-    helper Koi::DefinitionListHelper
+    include Koi::Controller
   end
 end
