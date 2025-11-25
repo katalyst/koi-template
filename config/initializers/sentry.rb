d0fa8dbc7f70067db18ab9cfbe9db21a0255bc76
Andy Williams <andy.williams@katalyst.com.au>
2025-11-25 15:07:21 +1030

    Update sentry config
    For compatibility with sentry v6

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,8 +8,21 @@ ActiveSupport.on_load(:active_record) do
     config.enabled_environments = %w[staging production]
     config.release              = File.read(version).strip.split("/").last if version.exist?
     filter             = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-    config.before_send = lambda do |event, _hint|
-      filter.filter(event.to_hash)
+    config.before_send          = lambda do |event, _hint|
+      # Sanitize extra data
+      if event.extra
+        event.extra = filter.filter(event.extra)
+      end
+      # Sanitize user data
+      if event.user
+        event.user = filter.filter(event.user)
+      end
+      # Sanitize context data (if present)
+      if event.contexts
+        event.contexts = filter.filter(event.contexts)
+      end
+      # Return the sanitized event object
+      event
     end
   end
 end
